1a90f7e2025051d5c3a614b3c9658230ad5a83af
pedrozorn <itt.pedrodev53@gmail.com>
2022-12-08 22:42:32 +0900

    test

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+      <!-- {{ users[0].id }} -->
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id}}, {{user.name}},{{user.company.name}}
+          error({ statusCode: e.response.status, message: e.message})
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/usersxxx'
+export default {
+  asyncData({ params }) {
+    return axios.get(url) 
+      .then((res) => {
+        return { users: res.data }
+      })
+      .catch((e => {
+        error({ users: e.response.status, message: 'Error!!!!!' })
+      }))
+  }
+}
+</script>
